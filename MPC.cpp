32e7a14c8c892551db8e79420b45019c99dd8f65
F3de22 <monettifederico22@gmail.com>
2025-05-22 19:27:47 +0200

    update

--- MPC.cpp
+++ MPC.cpp
@@ -1,19 +1,25 @@
 #include "MPC.hpp"
 
-MPC::MPC(){
-    Q = Q = Eigen::MatrixXd::Zero(nx, nx);
-    Q.diagonal() << 10, 10, 5, 2, 2;
+MPC::MPC() : solver_initialized(false){
+    Q = Eigen::MatrixXd::Zero(nx, nx);
+    Q.diagonal() << 10, 10, 0, 0, 0;
 
     R = Eigen::MatrixXd::Identity(nu, nu)*0.5;
-    R_delta = 1;
+    R_delta = Eigen::MatrixXd::Identity(1, 1); //moltiplicare per un valore se si vuole cambiare peso
 
     //TODO: settare limiti superiori e inferiori di ingressi e stati
     umin = Eigen::VectorXd::Zero(nu);
     umax = Eigen::VectorXd::Zero(nu);
-    umin[0] = -24;
-    umax[0] = 24;
-}
+    umin[0] = -24* 3.14 / 180.0;
+    umax[0] = 24* 3.14 / 180.0;
+    xmin = Eigen::VectorXd::Zero(nx);
+    xmax = Eigen::VectorXd::Zero(nx);
+    xmin[0] = 1.5;
+    xmax[0] = 1.5;
 
+    u_prev = Eigen::VectorXd::Zero(oc * nu);
+    numeri = load_data();//legge dati da file per le cornering stiffness
+}
 
 void MPC::updateDiscretization(double vx, double yaw_angle, double acc) {
     A = Eigen::MatrixXd::Zero(nx, nx);
@@ -22,7 +28,9 @@ void MPC::updateDiscretization(double vx, double yaw_angle, double acc) {
     double sin_th = sin(yaw_angle);
     double cos_th = cos(yaw_angle);
 
-    vector<vector<double>> numeri = load_data(); //legge dati da file per le cornering stiffness
+    std::pair<double, double> K_values = load_transfer(acc, numeri);
+    double Ka = K_values.first;  // Stiffness anteriore
+    double Kp = K_values.second; // Stiffness posteriore
 
     // Prima equazione
     A(0, 2) = -vx * sin_th; // ∂X'/∂θ
@@ -36,12 +44,12 @@ void MPC::updateDiscretization(double vx, double yaw_angle, double acc) {
     A(2, 4) = 1.0;
 
     // Quarta equazione
-    A(3, 5) = -(load_transfer(acc, numeri).first + load_transfer(acc, numeri).second) / (m * vx);
-    A(3, 4) = ((load_transfer(acc, numeri).second * lb - load_transfer(acc, numeri).first * la) / (m * vx)) - vx;
+    A(3, 5) = -(Kp + Ka) / (m * vx);
+    A(3, 4) = ((Kp * lb - Ka * la) / (m * vx)) - vx;
 
     // Quinta equazione
-    A(4, 3) = (-la * load_transfer(acc, numeri).first + lb * load_transfer(acc, numeri).second) / (Iz * vx);
-    A(4, 4) = -(la * la * load_transfer(acc, numeri).first + lb * lb * load_transfer(acc, numeri).second) / (Iz * vx);
+    A(4, 3) = (-la * Ka + lb * Kp) / (Iz * vx);
+    A(4, 4) = -(la * la * Ka + lb * lb * Kp) / (Iz * vx);
 
     // Matrice di controllo
     B(3, 0) = load_transfer(acc, numeri).first / m;
@@ -60,21 +68,25 @@ void MPC::updateDiscretization(double vx, double yaw_angle, double acc) {
 
 
 double MPC::compute(const Eigen::VectorXd& x0, vector<Point> waypoints, double yaw, double velocity){
-    // Costruzione pesi
-    Eigen::MatrixXd Q_big = Eigen::MatrixXd::Zero(op * nx, op * nx);
-    Eigen::MatrixXd R_big = Eigen::MatrixXd::Zero(oc * nu, oc * nu);
-    Eigen::MatrixXd Rd_big = Eigen::MatrixXd::Zero((oc - 1) * nu, (oc - 1) * nu);
+    // Costruzione matrici di peso a blocchi
+    Eigen::MatrixXd Q_blk = Eigen::MatrixXd::Zero(op * nx, op * nx);
+    Eigen::MatrixXd R_blk = Eigen::MatrixXd::Zero(oc * nu, oc * nu);
+    Eigen::MatrixXd Rd_blk = Eigen::MatrixXd::Zero((oc - 1) * nu, (oc - 1) * nu);
 
-    for (int i = 0; i < op; ++i) Q_big.block(i * nx, i * nx, nx, nx) = Q;
-    for (int i = 0; i < oc; ++i) R_big.block(i * nu, i * nu, nu, nu) = R;
-    for (int i = 0; i < oc - 1; ++i) Rd_big.block(i * nu, i * nu, nu, nu) = Rd;
+    for (int i = 0; i < op; ++i) 
+        Q_blk.block(i * nx, i * nx, nx, nx) = Q; // crea una matrice a blocchi diagonale (con la matrice Q su ogni blocco della diagonale)
+    for (int i = 0; i < oc; ++i) 
+        R_blk.block(i * nu, i * nu, nu, nu) = R;
+    for (int i = 0; i < oc - 1; ++i) 
+        Rd_blk.block(i * nu, i * nu, nu, nu) = R_delta;
 
-    // Costruzione Sx, Su
-    Eigen::MatrixXd Sx = Eigen::MatrixXd::Zero(op * nx, nx);
-    Eigen::MatrixXd Su = Eigen::MatrixXd::Zero(op * nx, oc * nu);
-    Eigen::MatrixXd A_power = Eigen::MatrixXd::Identity(nx, nx);
+    // Costruzione delle matrici predittive Sx, Su
+    Eigen::MatrixXd Sx = Eigen::MatrixXd::Zero(op * nx, nx); // Sx: Predizione dello stato futuro basata sullo stato iniziale (x0)
+    Eigen::MatrixXd Su = Eigen::MatrixXd::Zero(op * nx, oc * nu); // Su: Predizione dello stato futuro basata sulla sequenza di input (u)
+
+    Eigen::MatrixXd A_power = Eigen::MatrixXd::Identity(nx, nx); // Ad^0 = I
     for (int i = 0; i < op; ++i) {
-        Sx.block(i * nx, 0, nx, nx) = A_power;
+        Sx.block(i * nx, 0, nx, nx) = A_power; // Sx_i = Ad^i
         for (int j = 0; j <= i && j < oc; ++j) {
             Eigen::MatrixXd A_tmp = Eigen::MatrixXd::Identity(nx, nx);
             for (int k = 0; k < i - j; ++k)
@@ -84,56 +96,65 @@ double MPC::compute(const Eigen::VectorXd& x0, vector<Point> waypoints, double y
         A_power *= Ad;
     }
 
-    // Costruzione x_ref
+    // Costruzione x_ref_big
     Eigen::VectorXd x_ref_big = Eigen::VectorXd::Zero(op * nx);
     for (int i = 0; i < op; ++i) {
         int idx = std::min(i, static_cast<int>(waypoints.size()) - 1);
         x_ref_big.segment(i * nx, 2) << waypoints[idx].x, waypoints[idx].y;
-        // altri stati target (theta, vy, omega) a 0
+        // altri stati (theta, vy, omega) si lasciano a 0
     }
 
     // Funzione costo: 1/2 uᵀPu + qᵀu
-    Eigen::VectorXd x_pred = Sx * x0;
-    Eigen::MatrixXd P = Su.transpose() * Q_big * Su + R_big;
-    Eigen::VectorXd q = (x_pred - x_ref_big).transpose() * Q_big * Su;
+    Eigen::MatrixXd P = Su.transpose() * Q_blk * Su + R_blk;
+    Eigen::VectorXd x_pred = Sx * x0; // Predizione dello stato senza input (solo con x0)
+    Eigen::VectorXd q = (x_pred - x_ref_big).transpose() * Q_blk * Su;
 
     // Penalità su Δu = D*u - u_prev
     Eigen::MatrixXd D = Eigen::MatrixXd::Zero((oc - 1) * nu, oc * nu);
     for (int i = 0; i < oc - 1; ++i) {
-        D.block(i * nu, i * nu, nu, nu) = -Eigen::MatrixXd::Identity(nu, nu);
-        D.block(i * nu, (i + 1) * nu, nu, nu) = Eigen::MatrixXd::Identity(nu, nu);
+        // D contiene le differenze tra due ingressi successivi [uk - uk-1]
+        D.block(i * nu, i * nu, nu, nu) = -Eigen::MatrixXd::Identity(nu, nu); // -u_{k-1}// D contiene le differenze tra due ingressi successivi [uk - uk-1]
+        D.block(i * nu, (i + 1) * nu, nu, nu) = Eigen::MatrixXd::Identity(nu, nu); // +u_k
     }
-    P += D.transpose() * Rd_big * D;
-    q += (-u_prev.transpose() * Rd_big * D).transpose();
+    P += D.transpose() * Rd_blk * D;
+    q += (-u_prev.transpose() * Rd_blk * D).transpose();
 
     // Vincoli sullo sterzo
     Eigen::VectorXd lower_bound = Eigen::VectorXd::Constant(oc * nu, umin[0]);
     Eigen::VectorXd upper_bound = Eigen::VectorXd::Constant(oc * nu, umax[0]);
 
-    // Setup ottimizzatore OSQP
-    OsqpEigen::Solver solver;
+    // Setup ottimizzatore OSQP che calcola il controllo ottimo
+    if (!solver_initialized) {
     solver.settings()->setWarmStart(true);
     solver.settings()->setVerbosity(false);
+
     solver.data()->setNumberOfVariables(oc * nu);
     solver.data()->setNumberOfConstraints(oc * nu);
-
-    solver.data()->setHessianMatrix(P.sparseView());
-    solver.data()->setGradient(q);
-    solver.data()->setLinearConstraintsMatrix(Eigen::MatrixXd::Identity(oc * nu, oc * nu).sparseView());
-    solver.data()->setLowerBound(lower_bound);
-    solver.data()->setUpperBound(upper_bound);
+    Eigen::SparseMatrix<double> P_s = P.sparseView();
+    if (!solver.data()->setHessianMatrix(P_s)) return 0.0;
+    if (!solver.data()->setGradient(q)) return 0.0;
+    Eigen::MatrixXd A = Eigen::MatrixXd::Identity(oc * nu, oc * nu);
+    Eigen::SparseMatrix<double> A_s = A.sparseView();
+    if (!solver.data()->setLinearConstraintsMatrix(A_s)) return 0.0;
+    if (!solver.data()->setLowerBound(lower_bound)) return 0.0;
+    if (!solver.data()->setUpperBound(upper_bound)) return 0.0;
 
     if (!solver.initSolver()) {
         std::cerr << "OSQP solver init failed\n";
         return 0.0;
     }
 
-    // Warm start
-    if (u_prev.size() == oc * nu)
-        solver.setPrimalSolution(u_prev);
+    solver_initialized = true;
+    } else {
+        // aggiornamento dei dati per il warm start
+        Eigen::SparseMatrix<double> P_s = P.sparseView();
+        if (!solver.updateHessianMatrix(P_s)) return 0.0;
+        if (!solver.updateGradient(q)) return 0.0;
+        if (!solver.updateBounds(lower_bound, upper_bound)) return 0.0;
+    }
 
     if (!solver.solve()) {
-        std::cerr << "OSQP solve failed\n";
+        std::cerr << "OSQP: Risoluzione fallita\n";
         return 0.0;
     }
 
@@ -153,20 +174,18 @@ vector<vector<double>>MPC::load_data() {
         return {};
     }
 
-    vector<vector<double>> numeri(300, vector<double>(2));
-    char num1[20], num2[20];
-
     // Leggi i numeri dal file
-    for (int i = 0; i < 300; ++i) {
-        if (file >> num1 >> num2) {
-            numeri[i][0] = stod(num1);
-            numeri[i][1] = stod(num2);
+    vector<vector<double>> numeri;
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        double val1, val2;
+        if (ss >> val1 >> val2) {
+            numeri.push_back({val1, val2});
         } else {
-            cerr << "Errore nella lettura del file alla riga " << i + 1 << endl;
-            break;
+            cerr << "Errore nella lettura della riga: " << line <<endl;
         }
     }
-
     file.close();
 
     return numeri;
@@ -200,18 +219,10 @@ double interpolate(double Fx, const vector<vector<double>> &table) {
 
 pair<double,double> MPC::load_transfer(double acceleration, const vector<vector<double>> &numeri){
     //calcolo la forza che viene applicata perpendicolarmente sulle ruote
-    double Fant = ((m * 9.81 * lb) - (m * acceleration * Ycm)) / la+lb;
-    double Frear = ((m * 9.81 * la) + (m * acceleration * Ycm)) / la+lb;
+    double Fant = ((m * 9.81 * lb) - (m * acceleration * Ycm)) / (la+lb);
+    double Frear = ((m * 9.81 * la) + (m * acceleration * Ycm)) / (la+lb);
 
-    for (int i = 0; i < 300; ++i) {
-        cout << "Riga " << i+1 << " - Primo numero: " << numeri[i][0] << ", Secondo numero: " << numeri[i][1] << endl;
-    }
-
-    //ricerca di Ka e Kp (cornering stiffness) con interpolazione
-    double Kp = 0;
-    double Ka = 0;
-
-    // Interpolazione delle stiffness Ca e Cp
+    // Interpolazione delle stiffness Ka e Kp
     double Ka = interpolate(Fant, numeri);
     double Kp = interpolate(Frear, numeri);
 
